f9d382c5142c30eddf733f0fa906afd6f986b95e
m-zaitsev1-source <misharik15031@gmail.com>
2026-09-23 08:29:19 +0000

    ура написал

--- binary_search.h
+++ binary_search.h
@@ -1,3 +1,5 @@
 // binary_search.h
-size_t binary_search(void* arr[], size_t num_elements, const void* target);
-int compare(const void* a, const void* b);
+int binary_search(const void *arr, size_t size, size_t element_size, const void *target, int (*compare)(const void*, const void*));
+int compare_int(const void* a, const void* b);
+int compare_double(const void* a, const void* b);
+int compare_char(const void* a, const void* b); 
